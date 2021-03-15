d511333ce416103f00ff50d1df2077eef5c7246f
ctrelenberg <christian.trelenberg@gmail.com>
2021-03-14 18:40:27 -0700

    Added buttons to screen; need to add functionality

---
+++ button.py
@@ -0,0 +1,43 @@
+import pygame
+
+WHITE = (255, 255, 255)
+
+class Button(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, w, h, c):
+
+        super().__init__()
+
+        # Create transparent sprite surface to draw sprite on
+        self.image = pygame.Surface([w, h])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+
+        # Setting attributes
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+        # Setting colour
+        self.c = c
+
+        # Drawing box
+        self.draw()
+
+        # Setting bounding rectangle of the drawn line
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+    def draw(self):
+
+        # Draws line relative to the sprite surface
+        
+        c = self.c
+        x = self.x
+        y = self.y
+        w = self.w
+        h = self.h
+        pygame.draw.rect(self.image, c, [0, 0, w, h])
--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import pygame
 
 from node import Node
 from edge import Edge
+from button import Button
 from math import sqrt
 
 pygame.init()
 
 # Defining colours
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
 GREY = (210, 210, 210)
+D_GREY = (105, 105, 105)
 WHITE = (255, 255, 255)
+
+# Constants
+RADIUS = 30
 
 # Define and create window
 infoObject = pygame.display.Info()
@@ -27,32 +34,45 @@ pygame.display.set_caption("COMP361")
 all_nodes = pygame.sprite.Group()
 all_edges = pygame.sprite.Group()
 
+# Add buttons
+WIDTH = 80
+HEIGHT = 30
+buttons = pygame.sprite.Group()
+buttons.add(Button(10, SCREENHEIGHT-HEIGHT-10, WIDTH, HEIGHT, GREEN))
+buttons.add(Button(20+WIDTH, SCREENHEIGHT-HEIGHT-10, WIDTH, HEIGHT, RED))
+buttons.add(Button(10*3+WIDTH*2, SCREENHEIGHT-HEIGHT-10, WIDTH, HEIGHT, YELLOW))
+
 # Loop flag and setting up pygame clock
-carryOn = True
+appRunning = True
+stepAlg = False
+runAlg = False
 clock = pygame.time.Clock()
 
 # Counter for when to change node colour
 i = 1
 
-while carryOn:
+while appRunning:
+
+    #---------------------------------------------------------------------------
+    # User input handling
     
     for event in pygame.event.get(): # Check all user events
         
         if event.type == pygame.QUIT: # If user hit close, quit
-            carryOn = False
+            appRunning = False
 
-        elif event.type == pygame.MOUSEBUTTONDOWN: # If user clicks mouse, store position
+        elif event.type == pygame.MOUSEBUTTONDOWN: # If mouse down, store pos
             
-            pos = pygame.mouse.get_pos()
+            pos_down = pygame.mouse.get_pos()
 
         elif event.type == pygame.MOUSEBUTTONUP: # If user lifts mouse button...
 
-            new_pos = pygame.mouse.get_pos() # Get new position
+            pos_up = pygame.mouse.get_pos() # Get new position
             
-            dx = new_pos[0] - pos[0]
-            dy = new_pos[1] - pos[1]
+            dx = pos_up[0] - pos_down[0]
+            dy = pos_up[1] - pos_down[1]
 
-            if sqrt(dx**2 + dy**2) > 50: # If new position is more than 50 pixels away
+            if sqrt(dx**2 + dy**2) > 50: # If new pos is more than 50 pxls away
 
                 # Flags for points that occur within nodes
                 valid1 = False
@@ -61,18 +81,18 @@ while carryOn:
                 isDup = False
 
                 # Sorting the two points for ease of comparison
-                sorted_pos = sorted([pos, new_pos])
-                pos = sorted_pos[0]
-                new_pos = sorted_pos[1]
+                sorted_pos = sorted([pos_down, pos_up])
+                pos_down = sorted_pos[0]
+                pos_up = sorted_pos[1]
 
                 # Check that the endpoints occur within nodes
                 for node in all_nodes:
                     # Fix the endpoints to the centers of nodes if valid
-                    if node.inBounds(pos[0], pos[1]):
+                    if node.inBounds(pos_down[0], pos_down[1]):
                         valid1 = True
                         x1 = node.getX()
                         y1 = node.getY()
-                    if node.inBounds(new_pos[0], new_pos[1]):
+                    if node.inBounds(pos_up[0], pos_up[1]):
                         valid2 = True
                         x2 = node.getX()
                         y2 = node.getY()
@@ -100,38 +120,66 @@ while carryOn:
                         valid = False
                         
                 if valid:
-                    all_nodes.add(Node(pos[0], pos[1], 50, GREY))
+                    all_nodes.add(Node(pos[0], pos[1], RADIUS, GREY))
         
         elif event.type == pygame.KEYDOWN: # If a keypress is detected...
             
             if event.key == pygame.K_SPACE: # ... and it was the spacebar
             
-                for node in all_nodes: # Cycle the colour of the nodes. BLUE -> GREEN -> GREY -> BLUE
+                for node in all_nodes:
+                    # Cycle the colour of the nodes. BLUE -> GREEN -> GREY -> BLUE
                     if node.c == GREY:
                         node.setColour(BLUE)
                     elif node.c == BLUE:
                         node.setColour(GREEN)
                     elif node.c == GREEN:
                         node.setColour(GREY)
-    
-    if i % 60 == 0: # Every 60 frames, automatically cycle node colour
-        for node in all_nodes:
-            if node.c == GREY:
-                node.setColour(BLUE)
-            elif node.c == BLUE:
-                node.setColour(GREEN)
-            elif node.c == GREEN:
-                node.setColour(GREY)
-    
 
-    i += 1 # Update frame counter
+
+    #---------------------------------------------------------------------------
+    # Runtime "loop"
+    if runAlg:
+        if i % 60 == 0: # Every 60 frames, automatically cycle node colour
+            for node in all_nodes:
+                if node.c == GREY:
+                    node.setColour(BLUE)
+                elif node.c == BLUE:
+                    node.setColour(GREEN)
+                elif node.c == GREEN:
+                    node.setColour(GREY)
+        
+
+        i += 1 # Update frame counter
+
+
+    #---------------------------------------------------------------------------
+    # Draw screen
 
     screen.fill(WHITE) # Create blank screen
 
     all_edges.draw(screen) # Draw edges
     all_nodes.draw(screen) # Draw nodes on top of edges
+    buttons.draw(screen) # Draw buttons
+    
+    # Render text on top of buttons
+    text = pygame.font.SysFont('arial', 20)
+    
+    textSurf = text.render("Start", True, BLACK)
+    textRect = textSurf.get_rect()
+    textRect.center = ( 10 + WIDTH // 2, SCREENHEIGHT-HEIGHT-10 + HEIGHT // 2 )
+    screen.blit(textSurf, textRect)
 
-    pygame.display.flip() # Updates entire window
+    textSurf = text.render("Stop", True, BLACK)
+    textRect = textSurf.get_rect()
+    textRect.center = ( 20+WIDTH + WIDTH // 2, SCREENHEIGHT-HEIGHT-10 + HEIGHT // 2 )
+    screen.blit(textSurf, textRect)
+
+    textSurf = text.render("Step", True, BLACK)
+    textRect = textSurf.get_rect()
+    textRect.center = ( 10*3+WIDTH*2 + WIDTH // 2, SCREENHEIGHT-HEIGHT-10 + HEIGHT // 2 )
+    screen.blit(textSurf, textRect)
+
+    pygame.display.update() # Updates entire window
 
     clock.tick(60) # Cap framerate to 60
 
